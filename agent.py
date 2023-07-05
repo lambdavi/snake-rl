e5749218f273d2f5f7f9e854c0f9dcbc2ab1f944
Davide Buoso <daviibuoso@gmail.com>
2023-07-05 21:00:11 +0200

    better oop paradigm implemented

--- agent.py
+++ agent.py
@@ -5,9 +5,8 @@ from collections import deque
 import numpy as np
 import torch
 
-from helper import plot
 from model import Linear_QNet, QTrainer
-from snake_game_ai import Direction, Point, SnakeGameAI
+from snake_game_ai import Direction, Point
 
 # Constants
 MAX_MEMORY = 100_000
@@ -29,13 +28,6 @@ class Agent:
         self.model = Linear_QNet(11, 256, 3)
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
 
-        # TODO: create arguments
-        # self.resume_or_not()
-        
-    def resume_or_not(self):
-        model_folder_path = "./best_model"
-        if os.path.exists(model_folder_path):
-            self.model.load_state_dict(torch.load(os.path.join(model_folder_path, "model.pth")))
 
     def get_state(self, game):
         """
@@ -124,57 +116,3 @@ class Agent:
         
         return final_move
     
-def train():
-    """
-        Function to orchestrate all the training
-    """
-    # utilities for plotting
-    plot_scores = []
-    plot_mean_scores = []
-
-    # variables
-    total_score = 0
-    record = 0
-    agent = Agent()
-    game = SnakeGameAI()
-    
-    # Create the loop
-    while True:
-        # get old state
-        state_old = agent.get_state(game)
-
-        # get move based on the current state
-        final_move = agent.get_action(state_old)
-
-        # perform the move
-        reward, game_over, score = game.play_step(final_move)
-        # get new state
-        state_new = agent.get_state(game)
-
-        # train short memory
-        agent.train_short_memory(state_old, final_move, reward, state_new, game_over)
-
-        # remember
-        agent.remember(state_old, final_move, reward, state_new, game_over)
-
-        if game_over:
-            # train experience replay memory: train on all previous moves
-            game.reset()
-            agent.n_games += 1
-            agent.train_long_memory()
-
-            if score > record:
-                record = score
-                agent.model.save()
-
-            print(f"--- Game: {agent.n_games} - Score: {score} - Record: {record} ---")
-
-            plot_scores.append(score)
-            total_score += score
-            mean_score = total_score / agent.n_games
-            plot_mean_scores.append(mean_score)
-            plot(plot_scores, plot_mean_scores)
-
-
-if __name__ == "__main__":
-    train()
---
+++ main.py
@@ -0,0 +1,61 @@
+from helper import plot
+from agent import Agent
+from snake_game_ai import SnakeGameAI
+
+
+def train():
+    """
+        Function to orchestrate all the training
+    """
+    # utilities for plotting
+    plot_scores = []
+    plot_mean_scores = []
+
+    # variables
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+    
+    # Create the loop
+    while True:
+        # get old state
+        state_old = agent.get_state(game)
+
+        # get move based on the current state
+        final_move = agent.get_action(state_old)
+
+        # perform the move
+        reward, game_over, score = game.play_step(final_move)
+        # get new state
+        state_new = agent.get_state(game)
+
+        # train short memory
+        agent.train_short_memory(state_old, final_move, reward, state_new, game_over)
+
+        # remember
+        agent.remember(state_old, final_move, reward, state_new, game_over)
+
+        if game_over:
+            # train experience replay memory: train on all previous moves
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+                agent.model.save()
+
+            print(f"--- Game: {agent.n_games} - Score: {score} - Record: {record} ---")
+
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
+
+
+if __name__ == "__main__":
+    print("Game started.")
+    train()
+    print("Game finished")
